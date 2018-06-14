ba7c3d9e00b3683a0f0aba39d645cea3aef21b74
Gabriel Fernandes Leal <Gabriel Fernandes Leal>
2018-06-14 17:54:02 -0300

    FFT

---
+++ FFT.m
@@ -0,0 +1,33 @@
+#Fs = 1000;
+#t = 0:1/Fs:1-1/Fs;
+#x = cos(2*pi*100*t) + randn(size(t));
+#N = length(x);
+#xdft = fft(x);
+#xdft = xdft(1:N/2+1);
+
+x = [2, 2, 4, 4, 6];  #Sinal
+N = length(x);
+
+kernel = exp((-j*2*pi)/N);
+
+resultado = zeros (1, N);
+C = zeros (1, N);
+
+for(i = 1:N)
+  for(j = 1:N)
+    resultado(i) += x(j)*(kernel^((i-1)*(j-1)));  #Cálculo do X[k]
+  endfor
+    C(i) = ((resultado(i) * 2)/N);  #Cálculo do sinal real C[k]
+endfor
+
+subplot(3, 1, 1);
+stem(abs(fft(x)));
+title("FFT");
+
+subplot(3, 1, 2);
+stem(abs(resultado));
+title("FFT Manual");
+
+subplot(3, 1, 3);
+stem(abs(C));
+title("Sinal Real");
